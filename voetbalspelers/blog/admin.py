5679762e944a709714215a7187bf5453ae71f2a5
MALiebregts <mark.liebregts@gmail.com>
2024-03-05 20:34:08 +0100

    untracked files

--- voetbalspelers/blog/admin.py
+++ voetbalspelers/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
+from voetbalspelers import voetbalspelers
 from .models import Post
 
-admin.site.register(Post)
+admin.site.register(voetbalspelers)
 # Register your models here.
