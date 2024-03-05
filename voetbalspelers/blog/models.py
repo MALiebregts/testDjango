fa0f0f944005ae623646fd1b0b93772d0ab9ba89
MALiebregts <mark.liebregts@gmail.com>
2024-03-05 20:09:54 +0100

    toevoegen untracked files

--- voetbalspelers/blog/models.py
+++ voetbalspelers/blog/models.py
@@ -4,9 +4,9 @@ from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
+    naam_voetballer = models.CharField(max_length=50)
+    voetbalclub = models.CharField(max_length=50)
     autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
